ffe4cfa6721dc5815a994f69f8867c2169ecfb85
mlincon <mehdi.lincon@gmail.com>
2021-05-29 11:05:18 +0200

    add comments and basic logging

--- project3_data_warehouse/create_tables.py
+++ project3_data_warehouse/create_tables.py
@@ -2,27 +2,54 @@ import configparser
 import psycopg2
 from sql_queries import create_table_queries, drop_table_queries
 
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
 def drop_tables(cur, conn):
+    """
+    Before (re-)creating tables, drop the existing ones and start anew.
+
+    :param cur: database cursor object
+    :param conn: connection string to Redshift cluster
+    """
     for query in drop_table_queries:
+        logging.info(f'Executing query:\n {query}')
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    """
+    Given the provided destination, create tables based on predefined sql instructions.
+
+    :param cur: database cursor object
+    :param conn: connection string to Redshift cluster
+    """
     for query in create_table_queries:
+        logging.info(f'Executing query:\n {query}')
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+    Parse the config file to get the required credentials for connecting with Redshift.
+    Drop any existing tables and then (re-)create the required tables.
+    """
+
+    logging.info('Parsing config file for connection details')
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
+    logging.info('Attempting to connect to Redshift cluster')
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
     cur = conn.cursor()
+    logging.info('Cursor obtained')
 
+    logging.info('Dropping any pre-existing tables')
     drop_tables(cur, conn)
+
+    logging.info('Creating new tables')
     create_tables(cur, conn)
 
     conn.close()
--- project3_data_warehouse/etl.py
+++ project3_data_warehouse/etl.py
@@ -2,27 +2,54 @@ import configparser
 import psycopg2
 from sql_queries import copy_table_queries, insert_table_queries
 
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
 def load_staging_tables(cur, conn):
+    """
+    Load the JSON files from S3 to the specified Redshift cluster.
+
+    :param cur: database cursor object
+    :param conn: connection string to Redshift cluster
+    """
     for query in copy_table_queries:
+        logging.info(f'Executing query:\n {query}')
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """
+    Insert into fact and dimensional table from the staging tables
+
+    :param cur: database cursor object
+    :param conn: connection string to Redshift cluster
+    """
     for query in insert_table_queries:
+        logging.info(f'Executing query:\n {query}')
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+    Parse the config file to get the required credentials for connecting with Redshift.
+    Insert data into staging, fact and dimensional tables.
+    """
+
+    logging.info('Parsing config file for connection details')
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
+    logging.info('Attempting to connect to Redshift cluster')
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
     cur = conn.cursor()
-    
+    logging.info('Cursor obtained')
+
+    logging.info('Copying data from S3 to Redshift in staging tables')
     load_staging_tables(cur, conn)
+
+    logging.info('Inserting data in fact and dimension tables from staging')
     insert_tables(cur, conn)
 
     conn.close()
